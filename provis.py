f9be8f895d3c67548e1b500f49c1581ee1f26fc2
Justin W King <justinwking@me.com>
2025-02-28 01:05:00 -0500

    Update provis.py

--- provis.py
+++ provis.py
@@ -1,7 +1,4 @@
-import os, urllib.parse, json, subprocess, sys, shlex, pathlib
-
-
-
+import os, urllib.parse, json, subprocess, shlex, argparse
 
 Master_Dictionary ={'url':[],'commands':[]}
 
@@ -186,55 +183,79 @@ def provisioning_start(dict=Master_Dictionary):
 
     print("\nProvisioning complete:  Web UI will start now\n")
 
-default_workflow = Workflow("default")
-default_workflow.node(NodeEntry("https://github.com/ltdrdata/ComfyUI-Manager"))
-default_workflow.node(NodeEntry("https://github.com/cubiq/ComfyUI_essentials"))
-default_workflow.node(NodeEntry("https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite"))
+def create_workflow_dictionary():
+    workflow_dictionary ={}
+
+    default_workflow = Workflow("default")
+    default_workflow.node(NodeEntry("https://github.com/ltdrdata/ComfyUI-Manager"))
+    default_workflow.node(NodeEntry("https://github.com/cubiq/ComfyUI_essentials"))
+    default_workflow.node(NodeEntry("https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite"))
+    workflow_dictionary[default_workflow.type] = default_workflow
+
+    animate_diff = Workflow("animate_diff")
+    animate_diff.model(ModelEntry("animatediff_models","https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15.ckpt"))
+    workflow_dictionary[animate_diff.type] = animate_diff
+    
+    animate_sdxl = Workflow("animate_sdxld")
+    animate_sdxl.node(NodeEntry("https://github.com/Fannovel16/comfyui_controlnet_aux/"))
+    animate_sdxl.node(NodeEntry("https://github.com/FizzleDorf/ComfyUI_FizzNodes"))
+    animate_sdxl.node(NodeEntry("https://github.com/Kosinkadink/ComfyUI-AnimateDiff-Evolved"))
+    animate_sdxl.node(NodeEntry("https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet"))
+    animate_sdxl.model(ModelEntry("checkpoints","https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"))
+    animate_sdxl.model(ModelEntry("checkpoints","https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"))
+    animate_sdxl.model(ModelEntry("vae","https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors"))
+    animate_sdxl.model(ModelEntry("controlnet","https://huggingface.co/xinsir/controlnet-union-sdxl-1.0/resolve/main/diffusion_pytorch_model_promax.safetensors","controlnet-union-sdxl-1.0.safetensors"))
+    workflow_dictionary[animate_sdxl.type] = animate_sdxl
+
+    flux = Workflow("flux")
+    flux.model(ModelEntry("controlnet","https://huggingface.co/Shakker-Labs/FLUX.1-dev-ControlNet-Union-Pro/resolve/main/diffusion_pytorch_model.safetensors", "FLUX.1-dev-ControlNet-Union-Pro.safetensors"))
+    workflow_dictionary[flux.type] = flux
+
+    wan2=Workflow("wan2") # 2 text encoder, 1 image model, 1 text model, 1 vae
+    wan2.command(["pip","install","-U","sageattention"])
+    wan2.node(NodeEntry("https://github.com/kijai/ComfyUI-WanVideoWrapper.git"))
+    wan2.node(NodeEntry("https://github.com/kijai/ComfyUI-KJNodes.git"))
+    wan2.model(ModelEntry("text_encoders","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/open-clip-xlm-roberta-large-vit-huge-14_fp16.safetensors"))
+    wan2.model(ModelEntry("text_encoders","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/umt5-xxl-enc-fp8_e4m3fn.safetensors"))
+    wan2.model(ModelEntry("diffusion_models","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-I2V-14B-480P_fp8_e4m3fn.safetensors"))
+    wan2.model(ModelEntry("diffusion_models","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-T2V-1_3B_bf16.safetensors"))
+    wan2.model(ModelEntry("vae","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1_VAE_bf16.safetensors"))
+    workflow_dictionary[wan2.type] = wan2
+
+    wan2_large=Workflow("wan2_large") # 2 text encoder, 1 image model, 1 text model, 1 vae
+    wan2_large.command(["pip","install","-U","sageattention"])
+    wan2_large.node(NodeEntry("https://github.com/kijai/ComfyUI-WanVideoWrapper.git"))
+    wan2_large.node(NodeEntry("https://github.com/kijai/ComfyUI-KJNodes.git"))
+    wan2_large.model(ModelEntry("text_encoders","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/open-clip-xlm-roberta-large-vit-huge-14_visual_fp32.safetensors"))
+    wan2_large.model(ModelEntry("text_encoders","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/umt5-xxl-enc-bf16.safetensors"))
+    wan2_large.model(ModelEntry("diffusion_models","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-I2V-14B-720P_fp8_e4m3fn.safetensors"))
+    wan2_large.model(ModelEntry("diffusion_models","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-T2V-14B_fp8_e4m3fn.safetensors"))
+    wan2_large.model(ModelEntry("vae","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1_VAE_fp32.safetensors"))
+    wan2_large.command(["pip","install","-U","sageattention"])
+    workflow_dictionary[wan2_large.type] = wan2_large
+
+    return workflow_dictionary
+
+def create_help_file(dict):
+    help_string = ""
+    counter = 1
+    for key, values in dict.items():
+        help_string += (f"{counter}: {key}\n")
+        counter +=1
+    return help_string
 
 
-animate_diff = Workflow("animate")
-animate_diff.model(ModelEntry("animatediff_models","https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15.ckpt"))
-animate_sdxl = Workflow("animate")
-animate_sdxl.node(NodeEntry("https://github.com/Fannovel16/comfyui_controlnet_aux/"))
-animate_sdxl.node(NodeEntry("https://github.com/FizzleDorf/ComfyUI_FizzNodes"))
-animate_sdxl.node(NodeEntry("https://github.com/Kosinkadink/ComfyUI-AnimateDiff-Evolved"))
-animate_sdxl.node(NodeEntry("https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet"))
-animate_sdxl.model(ModelEntry("checkpoints","https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"))
-animate_sdxl.model(ModelEntry("checkpoints","https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"))
-animate_sdxl.model(ModelEntry("vae","https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors"))
-animate_sdxl.model(ModelEntry("controlnet","https://huggingface.co/xinsir/controlnet-union-sdxl-1.0/resolve/main/diffusion_pytorch_model_promax.safetensors","controlnet-union-sdxl-1.0.safetensors"))
-
-flux = Workflow("flux")
-flux.model(ModelEntry("controlnet","https://huggingface.co/Shakker-Labs/FLUX.1-dev-ControlNet-Union-Pro/resolve/main/diffusion_pytorch_model.safetensors", "FLUX.1-dev-ControlNet-Union-Pro.safetensors"))
-
-wan2=Workflow("wan2") # 2 text encoder, 1 image model, 1 text model, 1 vae
-wan2.command(["pip","install","-U","sageattention"])
-wan2.node(NodeEntry("https://github.com/kijai/ComfyUI-WanVideoWrapper.git"))
-wan2.node(NodeEntry("https://github.com/kijai/ComfyUI-KJNodes.git"))
-wan2.model(ModelEntry("text_encoders","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/open-clip-xlm-roberta-large-vit-huge-14_fp16.safetensors"))
-wan2.model(ModelEntry("text_encoders","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/umt5-xxl-enc-fp8_e4m3fn.safetensors"))
-wan2.model(ModelEntry("diffusion_models","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-I2V-14B-480P_fp8_e4m3fn.safetensors"))
-wan2.model(ModelEntry("diffusion_models","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-T2V-1_3B_bf16.safetensors"))
-wan2.model(ModelEntry("vae","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1_VAE_bf16.safetensors"))
-
-
-wan2_large=Workflow("wan2") # 2 text encoder, 1 image model, 1 text model, 1 vae
-wan2_large.command(["pip","install","-U","sageattention"])
-wan2_large.node(NodeEntry("https://github.com/kijai/ComfyUI-WanVideoWrapper.git"))
-wan2_large.node(NodeEntry("https://github.com/kijai/ComfyUI-KJNodes.git"))
-wan2_large.model(ModelEntry("text_encoders","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/open-clip-xlm-roberta-large-vit-huge-14_visual_fp32.safetensors"))
-wan2_large.model(ModelEntry("text_encoders","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/umt5-xxl-enc-bf16.safetensors"))
-wan2_large.model(ModelEntry("diffusion_models","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-I2V-14B-720P_fp8_e4m3fn.safetensors"))
-wan2_large.model(ModelEntry("diffusion_models","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-T2V-14B_fp8_e4m3fn.safetensors"))
-wan2_large.model(ModelEntry("vae","https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1_VAE_fp32.safetensors"))
-wan2_large.command(["pip","install","-U","sageattention"])
 
 if __name__ == "__main__":
-    #wan2.populate_dictionary()
-    #wan2_large.populate_dictionary()
-    #flux.populate_dictionary()
-    
-    default_workflow.populate_dictionary()
-    animate_sdxl.populate_dictionary()
+    workflow_dict = create_workflow_dictionary()
+    print(workflow_dict.keys())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--workflows', nargs='+',default=['default'],help=create_help_file(workflow_dict))
+    args = parser.parse_args()
+    for i in args.workflows:
+        if i in workflow_dict.keys():
+            print(workflow_dict[str(i)]) 
+            workflow_dict[str(i)].populate_dictionary()
     
     provisioning_start()
+    
